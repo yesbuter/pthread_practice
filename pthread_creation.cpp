78470252e0ba23d24d57baf67542e4bd5dacfc10
hecheng <hc9561@qq.com>
2018-03-19 20:46:25 +0800

    pthread creating test

---
+++ pthread_creation.cpp
@@ -0,0 +1,31 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_THREADS 115
+
+void *PrintHello(void *threadid)
+{
+    long tid;
+    tid = (long)threadid;
+    printf("Hello World! It's me, thread #%ld\n", tid);
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long t;
+    for(t = 0; t < NUM_THREADS; t++)
+    {
+        printf("In main: creating thread %ld\n", t);
+        rc = pthread_create(&threads[t], NULL, PrintHello, (void*)t);
+        if(rc)
+        {
+            printf("ERROR; the %ldth return code from pthread_create is %d\n", t, rc);
+            exit(-1);
+        }
+    }
+    pthread_exit(NULL);
+}
